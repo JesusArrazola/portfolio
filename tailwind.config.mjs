295f8b56edb44efdc9985d0fbfd2cd2b055362ac
Jesus Arrazola <arrazola69@hotmail.com>
2024-10-23 10:10:14 -0600

    23-10-2024/1

---
+++ tailwind.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: "selector",
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    fontFamily: {
+      heebo: ['"Heebo Variable"', "monospace"],
+    },
+    extend: {
+      colors: {
+        light: "#8695A4",
+        dark: "#21243D",
+        secondary: "#00A8CC",
+        primary: "#FF6464",
+      },
+    },
+  },
+  plugins: [],
+};
